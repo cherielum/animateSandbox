82e8da3e8b072df69b9815e59012da8b52cbf8f2
Cherie Lum <charismatic.cherie@gmail.com>
2017-11-25 22:46:22 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+$(function(){
+    $('#animateBtn').click(function(){
+        animate('#anim-head', 'bounce');
+        animate('#anim-body', 'rubberBand');  
+        return false; 
+    });
+
+    $('#animateBtn2').click(function(){
+        animate('header', 'slideOutUp');
+        setTimeout(function() {
+            $('header').css('visibility', 'hidden');
+        }, 1000);
+        return false; 
+    });
+
+
+    //animate
+    function animate(element, animation){
+        $(element).addClass('animated '+animation); 
+        var wait = setTimeout(function() {
+        $(element).removeClass('animated '+animation);
+        }, 1000);
+    
+    }
+})
+});
+
